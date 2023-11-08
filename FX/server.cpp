4a6e607c33e0cd65e3b255c2b0f14be8bc5531b9
Francois-xavier Coindreau <fcoindre@student.42.fr>
2023-11-08 15:31:09 +0100

    Implementation de poll semble fontionner mais utilisation de ioctl

--- FX/server.cpp
+++ FX/server.cpp
@@ -1,185 +1,216 @@
-//Example code: A simple server side code, which echos back the received message. 
-//Handle multiple socket connections with select and fd_set on Linux 
-#include <stdio.h> 
-#include <string.h> //strlen 
-#include <stdlib.h> 
-#include <errno.h> 
-#include <unistd.h> //close 
-#include <arpa/inet.h> //close 
-#include <sys/types.h> 
-#include <sys/socket.h> 
-#include <netinet/in.h> 
-#include <sys/time.h> //FD_SET, FD_ISSET, FD_ZERO macros 
 #include <iostream>
-	
-#define TRUE 1 
-#define FALSE 0 
-#define PORT 8888 
-	
-int main(int argc , char *argv[]) 
-{ 
-	int opt = TRUE; 
-	int master_socket , addrlen , new_socket , client_socket[30] , max_clients = 30 , activity, i , valread , sd; 
-	int max_sd; 
-	struct sockaddr_in address; 
-		
-	char buffer[1025]; //data buffer of 1K 
-		
-	//set of socket descriptors 
-	fd_set readfds; 
-		
-	//a message 
-	char *message = "HTTP/1.1 200 OK\nContent-Type: text/html\nContent-Length: 315\r\n\r\n<!DOCTYPE html><html><body><h1>My First Heading</h1><p>My first paragraph.</p></body></html>"; 
-	
-	//initialise all client_socket[] to 0 so not checked 
-	for (i = 0; i < max_clients; i++) 
-	{ 
-		client_socket[i] = 0; 
-	} 
-		
-	//create a master socket 
-	if( (master_socket = socket(AF_INET , SOCK_STREAM , 0)) == 0) 
-	{ 
-		perror("socket failed"); 
-		exit(EXIT_FAILURE); 
-	} 
-	
-	//set master socket to allow multiple connections , 
-	//this is just a good habit, it will work without this 
-	if( setsockopt(master_socket, SOL_SOCKET, SO_REUSEADDR, (char *)&opt, sizeof(opt)) < 0 ) 
-	{ 
-		perror("setsockopt"); 
-		exit(EXIT_FAILURE); 
-	} 
-	
-	//type of socket created 
-	address.sin_family = AF_INET; 
-	address.sin_addr.s_addr = INADDR_ANY; 
-	address.sin_port = htons( PORT ); 
-		
-	//bind the socket to localhost port 8888 
-	if (bind(master_socket, (struct sockaddr *)&address, sizeof(address))<0) 
-	{ 
-		perror("bind failed"); 
-		exit(EXIT_FAILURE); 
-	} 
-	printf("Listener on port %d \n", PORT); 
-		
-	//try to specify maximum of 3 pending connections for the master socket 
-	if (listen(master_socket, 3) < 0) 
-	{ 
-		perror("listen"); 
-		exit(EXIT_FAILURE); 
-	} 
-		
-	//accept the incoming connection 
-	addrlen = sizeof(address); 
-	puts("Waiting for connections ..."); 
-		
-	while(TRUE) 
-	{ 
-		//clear the socket set 
-		FD_ZERO(&readfds); 
-	
-		//add master socket to set 
-		FD_SET(master_socket, &readfds); 
-		max_sd = master_socket; 
-			
-		//add child sockets to set 
-		for ( i = 0 ; i < max_clients ; i++) 
-		{ 
-			//socket descriptor 
-			sd = client_socket[i]; 
-				
-			//if valid socket descriptor then add to read list 
-			if(sd > 0) 
-				FD_SET( sd , &readfds); 
-				
-			//highest file descriptor number, need it for the select function 
-			if(sd > max_sd) 
-				max_sd = sd; 
-		} 
-	
-		//wait for an activity on one of the sockets , timeout is NULL , 
-		//so wait indefinitely 
-		activity = select( max_sd + 1 , &readfds , NULL , NULL , NULL); 
-	
-		if ((activity < 0) && (errno!=EINTR)) 
-		{ 
-			printf("select error"); 
-		} 
-			
-		//If something happened on the master socket , 
-		//then its an incoming connection 
-		if (FD_ISSET(master_socket, &readfds)) 
-		{ 
-			if ((new_socket = accept(master_socket, (struct sockaddr *)&address, (socklen_t*)&addrlen))<0) 
-			{ 
-				perror("accept"); 
-				exit(EXIT_FAILURE); 
-			} 
-			
-			//inform user of socket number - used in send and receive commands 
-			printf("New connection , socket fd is %d , ip is : %s , port : %d \n" , new_socket , inet_ntoa(address.sin_addr) , ntohs (address.sin_port)); 
-		
-			//send new connection greeting message 
-			if( send(new_socket, message, strlen(message), 0) != strlen(message) ) 
-			{ 
-				perror("send"); 
-			} 
-				
-			puts("Welcome message sent successfully"); 
-				
-			//add new socket to array of sockets 
-			for (i = 0; i < max_clients; i++) 
-			{ 
-				//if position is empty 
-				if( client_socket[i] == 0 ) 
-				{ 
-					client_socket[i] = new_socket; 
-					printf("Adding to list of sockets as %d\n" , i);	
-					break; 
-				} 
-			} 
-		} 
-			
-		//else its some IO operation on some other socket 
-		for (i = 0; i < max_clients; i++) 
-		{ 
-			sd = client_socket[i]; 
-				
-			if (FD_ISSET( sd , &readfds)) 
-			{ 
-				//Check if it was for closing , and also read the 
-				//incoming message 
-				valread = read( sd , buffer, 1024);
-				std::cout << "valread = " << valread << std::endl;
-				if (valread == 0) 
-				{ 
-					//Somebody disconnected , get his details and print 
-					getpeername(sd , (struct sockaddr*)&address , (socklen_t*)&addrlen); 
-					printf("Host disconnected , ip %s , port %d \n" , inet_ntoa(address.sin_addr) , ntohs(address.sin_port)); 
-						
-					//Close the socket and mark as 0 in list for reuse 
-					close( sd ); 
-					client_socket[i] = 0; 
-				} 
-					
-				//Echo back the message that came in 
-				else
-				{ 
-					//set the string terminating NULL byte on the end 
-					//of the data read 
-					buffer[valread] = '\0'; 
+#include <netinet/in.h>
+#include <libc.h>
+#include <poll.h>
+#include <sys/socket.h>
 
-					printf("Host %d : %s \n" , i , buffer);
+#define SERVER_PORT 8888
+#define MAX_CLIENT 200
+#define BUFFER_SIZE 1024
 
-					send(sd , "HTTP/1.1 200 OK\nContent-Type: text/html\nContent-Length: 315\r\n\r\n<!DOCTYPE html><html><body><h1>My First Heading</h1><p>My first paragraph.</p></body></html>" , strlen(buffer) , 0 ); 
 
-				} 
-			} 
-		} 
-	} 
-		
-	return 0; 
-} 
+int main ()
+{
+	std::cout << "SERVER TEST" << std::endl;
+
+	int listen_sd;
+	int rc;
+	int on = 1;
+	struct sockaddr_in addr;
+	struct pollfd fds[MAX_CLIENT];
+	int nfds = 1;
+	int current_size = 0;
+	int i, j;
+	bool end_server = false;
+	int new_sd = -1;
+
+	char buffer[BUFFER_SIZE];
+	bool close_conn;
+
+	int len;
+	int len_response;
+
+	std::string page = "Hello";
+	len_response = page.length();	
+	std::string response = "HTTP/1.1 200 OK\nContent-Type: text/html\nContent-Length: " + std::to_string(len_response) + "\r\n\r\n" + page;
+
+	bool compress_array;
+
+	listen_sd = socket(AF_INET, SOCK_STREAM, 0);
+	if (listen_sd < 0)
+	{
+		std::cerr << "socket() failed" << std::endl;
+		exit (EXIT_FAILURE);
+	}
+
+	rc = setsockopt(listen_sd, SOL_SOCKET, SO_REUSEADDR, (char *)&on, sizeof(on));
+	if (rc < 0)
+	{
+		std::cerr << "setsockopt() failed" << std::endl;
+		close(listen_sd);
+		exit (EXIT_FAILURE);
+	}
+
+	
+	rc = ioctl(listen_sd, FIONBIO, (char *)&on);
+	if (rc < 0)
+	{
+		perror("ioctl() failed");
+		close(listen_sd);
+		exit(-1);
+	}
+	
+
+	bzero(&addr, sizeof(addr));
+	addr.sin_family = AF_INET;
+	addr.sin_addr.s_addr = htonl(INADDR_ANY);
+	addr.sin_port = htons(SERVER_PORT);
+
+	rc = bind (listen_sd, (struct sockaddr *)&addr, sizeof(addr));
+	if (rc < 0)
+	{
+		std::cerr << "bind() failed" <<std::endl;
+		close(listen_sd);
+		exit(EXIT_FAILURE);
+	}
+
+	rc = listen(listen_sd, 32);
+	if (rc < 0)
+	{
+		std::cerr << "listen() failed" <<std::endl;
+		close(listen_sd);
+		exit(EXIT_FAILURE);
+	}
+
+	bzero(fds, sizeof(fds));
+
+	fds[0].fd = listen_sd;
+	fds[0].events = POLLIN;
+
+	do
+	{
+		std::cout << "Waiting on poll()..." << std::endl;
+
+		rc = poll(fds, nfds, -1);
+		if (rc < 0)
+		{
+			std::cerr << " poll() failed" <<std::endl;
+			break;
+		}
+
+		current_size = nfds;
+		for (i = 0; i < current_size; i++)
+		{
+			std::cout << "debug " << i << std::endl;
+			if (fds[i].revents == 0)
+				continue;
+
+			if (fds[i].revents != POLLIN)
+			{
+				std::cout << " Error revents = " << fds[i].revents << std::endl;
+				end_server = true;
+				break;
+			}
+
+			if (fds[i].fd == listen_sd)
+			{
+				std::cout << " Listening socket is readable" << std::endl;
+				do
+				{
+					new_sd = accept(listen_sd, NULL, NULL);
+					if (new_sd < 0)
+					{
+						if (errno != EWOULDBLOCK)
+						{
+							std::cerr << " accept() failed" << std::endl;
+							end_server = true;
+						}
+						break;
+					}
+
+					std::cout << " New incoming connection - " << new_sd << std::endl;
+					fds[nfds].fd = new_sd;
+					fds[nfds].events = POLLIN;
+					nfds++;
+				}
+				while (new_sd != -1);
+			}
+			else
+			{
+				std::cout << " Descriptor " << fds[i].fd << " is readable " << std::endl;
+				close_conn = false;
+
+				//do
+				//{
+				rc = recv(fds[i].fd, buffer, sizeof(buffer), 0);
+				if (rc < 0)
+				{
+					if (errno != EWOULDBLOCK)
+					{
+						std::cerr << " recv() failed" << std::endl;
+						close_conn = true;
+					}
+					break;
+				}
+
+				if (rc == 0)
+				{
+					std::cout << " Connection closed" << std::endl;
+					close_conn = true;
+					break;
+				}
+
+				len = rc;
+				std::cout << len << " byte received" << std::endl;
+
+				rc = send (fds[i].fd, page.c_str(), len_response, 0);
+				std::cout << "send" << std::endl;
+				if (rc < 0)
+				{
+					std::cerr << " send() failed" << std::endl;
+					close_conn = true;
+					break;
+				}
+
+				close_conn = true;
+
+				//}
+				//while (true);
+
+				if (close_conn == true)
+				{
+					close(fds[i].fd);
+					fds[i].fd = -1;
+					compress_array = true;
+				}
+			}
+		}
+
+		if (compress_array == true)
+		{
+			compress_array = false;
+			for (i = 0; i < nfds; i++)
+			{
+				if (fds[i].fd == -1)
+				{
+					for(j = i; j < nfds-1; j++)
+					{
+						fds[j].fd = fds[j+1].fd;
+					}
+					i--;
+					nfds--;
+				}
+      		}
+		}
+	}
+	while (end_server == false);
+
+
+	for (i = 0; i < nfds; i++)
+	{
+	if(fds[i].fd >= 0)
+		close(fds[i].fd);
+	}
+
+	return 0;
+}
