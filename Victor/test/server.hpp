fb6954091e9936be1285b4efb3998df194038448
victorgiordani <victor.giordani@protonmail.com>
2023-11-08 22:27:28 +0100

    update

---
+++ Victor/test/server.hpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <vector>
+#include <unordered_map>
+#include <map>
+#include <algorithm>
+
+
+class server
+{
+	private:
+		std::string ip_address;
+		int port;
+		std::string ex;
+	public:
+		server();
+		~server();
+};
+
+server::server()
+{
+
+}
+
+server::~server()
+{
+
+}
