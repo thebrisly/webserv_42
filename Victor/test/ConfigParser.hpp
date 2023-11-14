1a5276e32d391abbf73c7e0c25bdb1ebb72a92bd
victorgiordani <victor.giordani@protonmail.com>
2023-11-14 13:48:55 +0100

    ready for merge

--- Victor/test/ConfigParser.hpp
+++ Victor/test/ConfigParser.hpp
@@ -7,7 +7,6 @@ class ConfigParser
 {
 	public:
 		ServerConfig parseConfig(const std::string& filename);
-
 	private:
 		void processLine(const std::string& key, const std::string& value, ServerConfig& config, std::string& currentContext);
 };
