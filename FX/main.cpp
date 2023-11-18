52f1d7e83496f1535345ad89a11c0ebdd716b7a3
Francois-xavier Coindreau <fcoindre@c1r5s7.42lausanne.ch>
2023-11-18 14:30:09 +0100

    Safety commit

--- FX/main.cpp
+++ FX/main.cpp
@@ -73,7 +73,7 @@ class Displayer
 class RunServer
 {
 	private :
-		ServerInitializer _server_init;
+		const int _socket_server;
 		std::map<int, Client> _map_client;
 
 		fd_set _readfds;
@@ -83,11 +83,16 @@ class RunServer
 		struct timeval _timeout;
 
 	public :
-		RunServer(const ServerInitializer server_init);
+		RunServer(const int server_init);
+		~RunServer();
 
+		void accept_new_connection();
+		void recvs_request();
+		void send_response();
+		void process();
 };
 
-RunServer::RunServer(ServerInitializer server_init) : _server_init(server_init)
+RunServer::RunServer(const int socket_server) : _socket_server(socket_server)
 {
 	FD_ZERO(&this->_writefds);
 	FD_ZERO(&this->_readfds);
@@ -96,7 +101,7 @@ RunServer::RunServer(ServerInitializer server_init) : _server_init(server_init)
 	FD_ZERO(&this->_cpy_writefds);
 
 
-	FD_SET(this->_server_init.get_sock_server(), &this->_cpy_readfds);
+	FD_SET(this->_socket_server, &this->_cpy_readfds);
 	
 
 	this->_timeout.tv_sec = 0;
@@ -115,7 +120,7 @@ void accept_new_connection(ServerInitializer & server_init, fd_set & cpy_readfds
 	map_clients.insert(std::pair<int, Client>(new_socket, Client(new_socket)));				
 }
 
-void read_socket (ServerInitializer & server_init, fd_set & cpy_readfds, fd_set & cpy_writefds ,std::map<int, Client> & map_clients, int i)
+void recvs_request (ServerInitializer & server_init, fd_set & cpy_readfds, fd_set & cpy_writefds ,std::map<int, Client> & map_clients, int i)
 {
 	char buffer[1025];
 	int size_read;
@@ -156,15 +161,16 @@ void read_socket (ServerInitializer & server_init, fd_set & cpy_readfds, fd_set 
 		map_clients[i].set_request(buffer);
 		map_clients[i].set_size_request(size_read);
 		map_clients[i].set_socket_mod(READ_M);
-		//std::cout << map_clients[i] << std::endl;
 	}
 
 }
 
-void send_response ()
+void send_response (int i, std::map<int, Client> & map_clients, fd_set & cpy_writefds)
 {
 
-	
+	std::string body = "<!DOCTYPE html><html><body><h1>My First Heading</h1><h2>My first paragraph.</h2><h2>My web server</h2></body></html>"; 
+	std::string header = "HTTP/1.1 200 OK\nContent-Type: text/html\nConnection: close\nContent-Length: " + std::to_string(body.length()) + "\r\n\r\n";
+	std::string response = header + body;
 
 	if (send(i , response.c_str(), response.length() , 0 ) == -1)
 	{
@@ -186,11 +192,75 @@ void send_response ()
 }
 
 
+void process (ServerInitializer & server_init, fd_set & cpy_readfds, fd_set & cpy_writefds, fd_set & readfds, fd_set & writefds ,std::map<int, Client> & map_clients)
+{
+	int i;
+	int max_sd;
+	struct timeval timeout;
+	int new_socket;
+
+	timeout.tv_sec = 0;
+	timeout.tv_usec = 100000;
+
+	readfds = cpy_readfds;
+	writefds = cpy_writefds;
+
+	int max_key = server_init.get_sock_server();
+	max_sd = server_init.get_sock_server();
+
+	for (std::map<int, Client>::iterator it = map_clients.begin(); it != map_clients.end(); ++it)
+	{
+		if (it->first > max_key)
+		{
+			max_key = it->first;
+		}
+	}
+
+	for ( i = 0 ; i < MAX_CLIENT ; i++)
+	{
+		if (FD_ISSET(i, &readfds) && i > max_sd)
+		{
+			max_sd = i;
+		}
+		else if (FD_ISSET(i, &writefds) && i > max_sd)
+		{
+			max_sd = i;
+		}
+	}
+
+	if (max_key != max_sd)
+	{
+		std::cout << RED << "---------ERROR--------" << RESET <<std::endl;
+	}
+
+
+	/* select() Delete from readfds and writefds all the sockets not "ready" for an I/O operation. */
+	if (select(max_sd +1 , &readfds, &writefds, NULL, &timeout) < 0) 
+	{ 
+		std::cerr << RED << "ERROR : " << RESET;
+		perror("select");
+	} 
+
+	for (i = 0; i < max_sd + 1; i++) 
+	{
+		if (FD_ISSET(i, &readfds) && i == server_init.get_sock_server()) 
+		{
+			accept_new_connection(server_init, cpy_readfds, new_socket, map_clients);
+		}
+		else if (FD_ISSET(i, &readfds)) 
+		{
+			recvs_request (server_init, cpy_readfds, cpy_writefds , map_clients, i);
+		}
+		else if (FD_ISSET(i, &writefds))
+		{
+			send_response(i, map_clients, cpy_writefds);
+		}
+	}	
+
+}
+
 int main() 
 { 
-	int new_socket, i; 
-	int max_sd; 
-	//char buffer[1025];
 
 	fd_set readfds;
 	fd_set writefds;
@@ -204,103 +274,23 @@ int main()
 	FD_ZERO(&cpy_readfds);
 	FD_ZERO(&cpy_writefds);
 
-	std::ofstream outputFile("log.txt");
-
-	struct timeval timeout;
-
-	timeout.tv_sec = 0;
-	timeout.tv_usec = 100000;
-	
-	std::string body = "<!DOCTYPE html><html><body><h1>My First Heading</h1><h2>My first paragraph.</h2><h2>My web server</h2></body></html>"; 
-	std::string header = "HTTP/1.1 200 OK\nContent-Type: text/html\nConnection: close\nContent-Length: " + std::to_string(body.length()) + "\r\n\r\n";
-	std::string response = header + body;
+	//std::ofstream outputFile("log.txt");
 	
 	std::map<int, Client> map_clients;
 
 	try
 	{
-
 		ServerInitializer server_init(PORT, SIZE_WAITING_LIST);
 		server_init.bind_listen_socket_serv();
 
 		signal(SIGPIPE, sigpipeHandle);
 
 		FD_SET(server_init.get_sock_server(), &cpy_readfds);
-
 		std::cout << MAGENTA << "Listening on socket " << server_init.get_sock_server() << " bind with port " << PORT << "\033[0m" << std::endl;
 		
 		while(42) 
 		{
-			readfds = cpy_readfds;
-			writefds = cpy_writefds;
-
-			int max_key = server_init.get_sock_server();
-			max_sd = server_init.get_sock_server();
-
-			for (std::map<int, Client>::iterator it = map_clients.begin(); it != map_clients.end(); ++it)
-			{
-				//std::cout << it->second << std::endl;
-				if (it->first > max_key)
-				{
-					max_key = it->first;
-				}
-			}
-
-			for ( i = 0 ; i < MAX_CLIENT ; i++)
-			{
-				if (FD_ISSET(i, &readfds) && i > max_sd)
-				{
-					max_sd = i;
-				}
-				else if (FD_ISSET(i, &writefds) && i > max_sd)
-				{
-					max_sd = i;
-				}
-			}
-
-			if (max_key != max_sd)
-			{
-				std::cout << RED << "---------ERROR--------" << RESET <<std::endl;
-			}
-
-
-			/* select() Delete from readfds and writefds all the sockets not "ready" for a I/O operation. */
-			if (select(max_sd +1 , &readfds, &writefds, NULL, &timeout) < 0) 
-			{ 
-				std::cerr << RED << "ERROR : " << RESET;
-				perror("select");
-			} 
-
-			for (i = 0; i < max_sd + 1; i++) 
-			{
-				if (FD_ISSET(i, &readfds) && i == server_init.get_sock_server()) 
-				{
-					accept_new_connection(server_init, cpy_readfds, new_socket, map_clients);
-				}
-				else if (FD_ISSET(i, &readfds)) 
-				{
-					read_socket (server_init, cpy_readfds, cpy_writefds , map_clients, i);
-				}
-				else if (FD_ISSET(i, &writefds))
-				{
-					if (send(i , response.c_str(), response.length() , 0 ) == -1)
-					{
-						std::cerr << RED << "ERROR : " << RESET;
-						perror("send");
-					}
-					
-					std::cout << GREEN << "Sent response of " << response.length() << " characters to client "<< i << RESET <<std::endl;
-					
-					if (close (i) < 0)
-					{
-						std::cerr << RED << "ERROR : " << RESET;
-						perror("close");
-					}
-					std::cout << YELLOW << "Client " << i << " disconected." << RESET <<std::endl;				
-					FD_CLR(i, &cpy_writefds);
-					map_clients.erase(i);
-				}
-			}
+			process (server_init, cpy_readfds, cpy_writefds, readfds, writefds , map_clients);
 		}
 	}
 	catch(std::exception& e)
