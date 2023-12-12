c7615c0db4c2c5a7420c95e2c282b02d123cbc7e
Victor Giordani <vgiordan@student.42lausanne.ch>
2023-12-12 13:30:26 +0100

    added directory listing

--- sources/ServerConfig.cpp
+++ sources/ServerConfig.cpp
@@ -1,8 +1,7 @@
 #include "../includes/ServerConfig.hpp"
 #include "../includes/Color.hpp"
-
 // Constructor
-ServerConfig::ServerConfig() : port(0), ip_address(""), max_body_size(0), server_name(""), root("") {}
+ServerConfig::ServerConfig() : port(0), ip_address(""), max_body_size(0), server_name(""), root(""), default_file("") {}
 
 // Destructor
 ServerConfig::~ServerConfig() {}
@@ -14,7 +13,9 @@ ServerConfig::ServerConfig(const ServerConfig &other) : port(other.port),
 	server_name(other.server_name),
 	error_pages(other.error_pages),
 	root(other.root),
-	routes(other.routes) {}
+	routes(other.routes),
+    default_file(other.default_file)
+    {}
 
 // Copy Assignment Operator
 ServerConfig &ServerConfig::operator=(const ServerConfig &other) {
@@ -24,6 +25,7 @@ ServerConfig &ServerConfig::operator=(const ServerConfig &other) {
         max_body_size = other.max_body_size;
         server_name = other.server_name;
         error_pages = other.error_pages;
+        default_file = other.default_file;
         root = other.root;
         routes = other.routes;
     }
@@ -35,6 +37,9 @@ void ServerConfig::setPort(unsigned long p) { port = p; }
 void ServerConfig::setIPAddress(const std::string& ip) { ip_address = ip; }
 void ServerConfig::setMaxBodySize(unsigned long size) { max_body_size = size; }
 void ServerConfig::setServerName(const std::string& name) { server_name = name; }
+void ServerConfig::setDefaultFile(const std::string& path) { 
+    this->default_file = path;
+    std::cout << YELLOW << "Default file set to: " << getDefaultFile() << RESET << std::endl;}
 void ServerConfig::setErrorPage(int errorCode, const std::string& pagePath) { error_pages[errorCode] = pagePath; }
 void ServerConfig::setRoot(const std::string& r) { root = r; }
 void ServerConfig::addRoute(const RouteConfig& route) { routes.push_back(route); }
@@ -44,6 +49,7 @@ unsigned long ServerConfig::getPort() const { return port; }
 std::string ServerConfig::getIPAddress() const { return ip_address; }
 unsigned long ServerConfig::getMaxBodySize() const { return max_body_size; }
 const std::string& ServerConfig::getServerName() const { return server_name; }
+const std::string& ServerConfig::getDefaultFile() const { return default_file; }
 const std::map<int, std::string>& ServerConfig::getErrorPages() const { return error_pages; }
 const std::string& ServerConfig::getRoot() const { return root; }
 const std::vector<RouteConfig>& ServerConfig::getRoutes() const { return routes; }
@@ -72,12 +78,12 @@ RouteConfig ServerConfig::getRoute(const std::string& path) const
 
 std::ostream& operator<<(std::ostream& os, const ServerConfig &config)
 {
-	os << CYAN <<"\n   server name : " << RESET<< config.getServerName() << std::endl;
-	os << CYAN <<"     ip address : " << RESET<< config.getIPAddress() << std::endl;
-	os << CYAN <<"           port : " << RESET<< config.getPort() << std::endl;
-	os << CYAN <<"  max body size : " << RESET<< config.getMaxBodySize() << std::endl;
-	os << CYAN <<"           root : " << RESET<< config.getRoot() << std::endl;
-
+	os << CYAN << "\n    server name : " << RESET << config.getServerName() << std::endl;
+	os << CYAN << "     ip address : " << RESET << config.getIPAddress() << std::endl;
+    os << CYAN << "   default file : " << RESET << config.getDefaultFile() << std::endl;
+	os << CYAN << "           port : " << RESET << config.getPort() << std::endl;
+	os << CYAN << "  max body size : " << RESET << config.getMaxBodySize() << std::endl;
+	os << CYAN << "           root : " << RESET << config.getRoot() << std::endl;
     for (std::map<int, std::string>::const_iterator it = config.getErrorPages().begin(); it!= config.getErrorPages().end(); ++it)
     {
         os << CYAN << "     error code : " << RESET << it->first << YELLOW << "   error page : " << RESET << it->second << std::endl;
@@ -92,7 +98,6 @@ std::ostream& operator<<(std::ostream& os, const ServerConfig &config)
             os << *it2 << " ";
         }
         os << std::endl;
-        os << CYAN << " route default file : " << RESET << it->default_file << std::endl;
         os << CYAN << "  directory listing : " << RESET << it->directory_listing << std::endl;
         os << BLUE << "        redirections: " << RED << it->redirection << RESET << std::endl;
         // for (std::vector<std::pair<std::string, std::string> >::const_iterator it2 = it->redirections.begin(); it2!= it->redirections.end(); ++it2)
